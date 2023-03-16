b4a7f6bb8435fee5b6b9c93742b7e57b4a6c4640
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-16 19:42:47 -0300

    Task 100 | Project 2 Development of the coding

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _realloc - Reallocates memory block using malloc and free.
+ *
+ * @ptr: Pointer poinnting to the memory previosluy
+ * allocated with malloc(old_size).
+ * @old_size: Size in bytes, of allocated space for ptr.
+ * @new_size: New size, in bytes of the new memory block.
+ * Return: void.
+ *
+ * Author: Jeremias Erba.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int iteratorPtr = 0;
+	char *NewOldPtr = NULL;
+
+	NewOldPtr = (char *)malloc(old_size * new_size);
+	/* Conditions in order */
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size > old_size)
+	{
+		free(ptr);
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	else if (ptr == NULL)
+	{
+		free(ptr);
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (iteratorPtr = 0; iteratorPtr < (old_size * new_size); iteratorPtr++)
+		NewOldPtr[iteratorPtr] = ptr[iteratorPtr];
+	return (NewOldPtr);
+}
