b6374f620c321b1e5be4982d4e5db04d4dc3ee66
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-19 15:45:45 -0300

    Improving repo organization

---
+++ 1x01-malloc_free/°!
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * strtow - Split a string into words.
+ *
+ * @str: Char pointer.
+ * Return: Char.
+ *
+ * Author: Jeremias Erba.
+ */
+
+char **strtow(char *str)
+{
+
+}
--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -22,11 +22,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (ptr == NULL)
+	if (!(ptr))
 		return (malloc(new_size));
-
-	if (new_size == old_size)
-		return (ptr);
 
 	/* Conditions in more of one line*/
 	if (new_size == 0 && ptr != NULL)
