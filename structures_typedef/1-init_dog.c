b11677fdbf26cae4a5e59e94ad584937b69b80bf
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-20 11:04:52 -0300

    Task 1 - Project 1 1 - Create constructor. 2 - Add conditions if is NULL pointer.

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -15,14 +15,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
 
 	if (d == NULL)
 	{
 		printf("Ok\n");
 		exit(0);
 	}
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 }
