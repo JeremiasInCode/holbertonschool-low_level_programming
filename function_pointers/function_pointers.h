f8d4feb01f827540156747aa1d12e5b52442d35e
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-21 14:50:30 -0300

    Update .h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -7,4 +7,7 @@ void print_name(char *name, void (*f)(char *));
 /* -- Task 1 -- */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/* -- Task 2 -- */
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
