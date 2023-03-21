f6573028b1e07ff4f73af3ded041cf1ed5e6ca5f
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-21 12:49:03 -0300

    Task 1 - Project 2 Development of the coding: 1 - Verify that the function is not null. 2 - Iterate and for each iteration, called the function. 3 - Add description.

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 
+/**
+ * array_iterator - Executed a function given as a parameter on each element of an array.
+ * 
+ * @array: Data structure.
+ * @size: Size of the array.
+ * @action: function passed by parameters.
+ *
+ * Return: void.
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0, socket;
