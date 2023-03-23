9c177878d38b97b4b2179deb5516d2837bb2f6e0
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-23 11:04:43 -0300

    Update variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,16 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
 
+/* -- Task 0 -- */
 int sum_them_all(const unsigned int n, ...);
+
+/* -- Task 1 -- */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+/* -- Task 2 -- */
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/* -- Task 3 -- */
+void print_all(const char * const format, ...);
 
 #endif
