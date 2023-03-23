ddc060f9f7e8d467c9ded38ddec5224843caa79e
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-23 14:10:26 -0300

    Task 2 - Project 2 Development of the script. 1) Add (nill).

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_strings - Print string.
+ *
+ * @separator: Separator for words.
+ * @n: cant of arguments.
+ *
+ * Return: void.
+ */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
@@ -13,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (; i < n; i++)
 	{
 		character = va_arg(list, char*);
-		printf("%s", character);
+		(character == NULL) ? printf("(nill)") : printf("%s", character);
 
 		if ((i + 1 != n) && (separator != NULL))
 			printf("%s", separator);
