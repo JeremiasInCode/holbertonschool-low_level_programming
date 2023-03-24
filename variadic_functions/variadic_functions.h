40e18ea9d6431e6f75fd9a8b44ffdf64f8eed1ea
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-24 09:10:08 -0300

    Update .h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _variadic_functions_h_
 #define _variadic_functions_h_
+#include <stdarg.h>
 
 /* -- Task 0 -- */
 int sum_them_all(const unsigned int n, ...);
@@ -12,5 +13,19 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 /* -- Task 3 -- */
 void print_all(const char * const format, ...);
+
+
+typedef struct structure
+{
+	char option;
+	int(*function)(va_list); 
+	/* Function to extra functions. (int,char,float etc.)*/
+}op_t;
+
+
+int function_char(va_list params);
+int function_integer(va_list params);
+int function_float(va_list);
+int function_string(va_list);
 
 #endif
