1c432adb20abac6b616f4a5e4ae80c48e5ea5858
JereDev19 <jeremiaserba7894@gmail.com>
2023-04-03 15:38:03 -0300

    Task 4 Project 3

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,9 +1,18 @@
 #include "lists.h"
 
+/**
+ * free_list - free a list.
+ *
+ * @head: pointer to the node.
+ *
+ * Return: void.
+ */
+
 void free_list(list_t *head)
 {
-	if (head)
+	while (head)
 	{
+		/*Llamada recursiva para que free de el ultimo hacia el primero*/
 		free_list(head->next);
 		if (head->str)
 		{
