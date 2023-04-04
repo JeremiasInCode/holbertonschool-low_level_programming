bb24584ea8bd4e539d04d1c3ebfdaadf1ae67b93
JereDev19 <jeremiaserba7894@gmail.com>
2023-04-04 10:19:20 -0300

    Task 5 - Project 2 - Development of the script.

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Get a node through a index.
+ *
+ * @head: Pointer to the first node.
+ * @index: index of the node.
+ *
+ * Return: Addres of node found.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+
+	while (i != index)
+	{
+		head = head->next;
+		i++;
+	}
+
+	return (head);
+}
