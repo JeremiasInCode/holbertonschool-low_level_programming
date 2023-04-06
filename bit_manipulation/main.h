b34cdd1ff771a518329f27d59cf69245d475a3ee
JereDev19 <jeremiaserba7894@gmail.com>
2023-04-06 12:03:47 -0300

    Update .h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,8 +1,11 @@
 #ifndef _main_h_
 #define _main_h_
 #include <stdio.h>
+#include <stdarg.h>
 
 /* -- Project 3 -- */
+
+int _putchar(char c);
 
 /*Task 1*/
 unsigned int binary_to_uint(const char *b);
