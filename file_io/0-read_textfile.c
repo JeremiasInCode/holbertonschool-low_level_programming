cad772bf481d42244f224dc06419b225712cdca8
JereDev19 <jeremiaserba7894@gmail.com>
2023-04-11 10:54:29 -0300

    Development of the script - Using function extra for free buffer. - Implement of function in the some file.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 /**
+ * freeBuffer - Function that free a buffer.
+ *
+ * @buffer: Buffer to free.
+ */
+
+void freeBuffer(char **buffer)
+{
+	((*buffer) ? free(*buffer), *buffer = NULL : NULL);
+}
+
+/**
  * read_textfile - Read a text file and print.
  *
  * @filename: name of file.
