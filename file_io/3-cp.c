4f56048efcea02ebef47b6f490b6470bb996ee43
JereDev19 <jeremiaserba7894@gmail.com>
2023-04-11 14:09:59 -0300

    Task 3 - Project 0 - Fix in the coding. 1) Condition in the while. 2) Fix in the limit of lines. 3) Fix in % of dprintf. 4) Change permission of file, O_WRONLY.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fs = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
+	fs = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fs == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
