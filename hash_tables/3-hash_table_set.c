582360b3d7b35ede297dfdf20e929e3ea03d0bed
JereDev19 <jeremiaserba7894@gmail.com>
2023-04-13 11:51:26 -0300

    Task 3 - Project 2 - Hash Tables - Add return. - Refactoring code.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,21 +17,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int htIndex = 0;
 	hash_node_t *htNode = NULL;
 
-	if (!key || !value || !ht)
+	if (strcmp(key, "") == 0)
+		return (0);
+
+	if (!ht)
 		return (0);
 
 	htIndex = key_index((const unsigned char *) key, ht->size);
 	htNode = ht->array[htIndex];
 
-	/*If exists a node in the corresponding index with the same key*/
 	if (htNode && strcmp(htNode->key, key) == 0)
 	{
 		free(htNode->value);
 		htNode->value = strdup(value);
 		return (1);
 	}
-	/*Otherwise, I create a new node in that index.*/
-	else if (htNode == NULL && strcmp(htNode->key, key) != 0)
+	else
 	{
 		htNode = malloc(sizeof(hash_node_t));
 		if (!htNode)
@@ -41,7 +42,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (!htNode->key)
 			return (0);
 
-		/* Connect node */
 		htNode->value = strdup(value);
 		htNode->next = ht->array[htIndex];
 		ht->array[htIndex] = htNode;
